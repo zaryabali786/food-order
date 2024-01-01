fcf6f845a29a086a35424a3103b293d6f0cf1f8c
Front End Developer <front-end5@clarisync.comm>
2024-01-01 16:41:33 +0500

    new

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,75 +1,44 @@
-import { clientPromise } from "@/libs/mongo";
-
+import { connectMongoDB } from "@/libs/mongoConnect";
 import {User} from '@/models/User';
 import NextAuth, {getServerSession} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-// export const authOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {},
-
-//       async authorize(credentials) {
-//         const { email, password } = credentials;
-
-//         try {
-//           await connectMongoDB();
-//           const user = await User.findOne({ email });
-
-//           if (!user) {
-//             return null;
-//           }
-
-//           const passwordsMatch = await bcrypt.compare(password, user.password);
-
-//           if (!passwordsMatch) {
-//             return null;
-//           }
-
-//           return user;
-//         } catch (error) {
-//           console.log("Error: ", error);
-//         }
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: "jwt",
-//   },
-//   secret: process.env.SECRET,
-
-// };
 export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
-
     CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
+      name: "credentials",
+      credentials: {},
 
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({email});
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
+      async authorize(credentials) {
+        const { email, password } = credentials;
 
-        if (passwordOk) {
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordsMatch) {
+            return null;
+          }
+
           return user;
+        } catch (error) {
+          console.log("Error: ", error);
         }
-
-        return null
-      }
-    })
+      },
+    }),
   ],
-};
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.SECRET,
 
+};
 
 export async function isAdmin() {
   const session = await getServerSession(authOptions);
@@ -91,6 +60,7 @@ export async function isAdmin() {
 }
 
 const handler = NextAuth(authOptions);
+// const handler1 = NextAuth(authOptions);
 
 
 export { handler as GET, handler as POST };
