b3874f552ce951b864d82d854f9dd6e7aa657636
Front End Developer <front-end5@clarisync.comm>
2024-01-04 13:25:08 +0500

    test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -47,15 +47,15 @@ export const authOptions = {
 };
 
 export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
+  // const session = await getServerSession(authOptions);
+  // const userEmail = session?.user?.email;
+  // if (!userEmail) {
+  //   return false;
+  // }
+  // const userInfo = await UserInfo.findOne({email:userEmail});
+  // if (!userInfo) {
+  //   return false;
+  // }
   return true;
 }
 
